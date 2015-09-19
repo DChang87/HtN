512fb35ed90f9821a095d7899a1ac901d4b0742a
orodrigu <oswinrods@gmail.com>
2015-09-19 15:39:39 -0400

    Oswin's first commit

---
+++ pebble/src/js/talk-to-phone.js
@@ -0,0 +1,29 @@
+/*function mssgReceivedSuccess(mssg) {
+  // We will request the weather here
+}
+
+function mssgReceivedError(err) {
+  console.log('Error requesting location!');
+}
+
+function getMssg() {
+  
+}
+
+// Listen for when the watchapp is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log('PebbleKit JS ready!');
+
+    // Get initial message
+    getMssg;
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log('AppMessage received!');
+  }                     
+);
+*/
